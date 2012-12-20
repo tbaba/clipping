a4847d433242b3f799117ad5fad74af87e27c119
Tatsuro Baba <harakirisoul@gmail.com>
2012-12-20 19:06:35 +0900

    rails new clipping -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clipping::Application.config.secret_token = '66cb2b1d2c649c1e710731e9fdb3d0a4d4336cdd77a0ad6b8f87d1ae3e8eeab4e349a023a089ce351df2baf1f20d94ed7766ea1320d3e83d8d3efd64000299ce'
